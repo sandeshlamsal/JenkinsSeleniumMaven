69afdbd13386ebf0f7226fa0108e60bfba8cdcd3
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2018-07-07 00:39:40 -0500

    2nd commit

--- src/test/java/com/san/test/SeleniumTest.java
+++ src/test/java/com/san/test/SeleniumTest.java
@@ -2,6 +2,7 @@ package com.san.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,6 +14,11 @@ public class SeleniumTest {
 	void OpenBrowser() throws InterruptedException{
 		System.out.println("browser opened");
 		wd.get("http://www.facebook.com");
+	}
+	
+	@Test
+	void test1(){
+		Assert.assertTrue(true);
 	}
 	
 	@BeforeTest
