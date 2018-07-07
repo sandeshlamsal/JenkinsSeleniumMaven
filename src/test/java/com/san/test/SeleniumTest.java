ba45a639711e074511fcef34eb3f52951815fffe
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2018-07-06 23:07:10 -0500

    first commit

---
+++ src/test/java/com/san/test/SeleniumTest.java
@@ -0,0 +1,30 @@
+package com.san.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+ 
+public class SeleniumTest {
+	WebDriver wd;
+	
+	@Test
+	void OpenBrowser() throws InterruptedException{
+		System.out.println("browser opened");
+		wd.get("http://www.facebook.com");
+	}
+	
+	@BeforeTest
+	void SetUp(){
+		 System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		 wd=new ChromeDriver();		
+	}
+	
+	@AfterTest
+	void EndTest(){
+		wd.close();
+		System.out.println("Browser closed");
+	}
+	
+}
